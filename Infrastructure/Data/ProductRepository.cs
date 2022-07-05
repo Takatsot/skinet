1b39657102c7416ecb376ec32647acc152a79427
Sifiso Tsukulu <sjtsukulu@shoprite.co.za>
2022-07-05 10:28:51 +0200

    End of Section 4

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -25,7 +25,7 @@ namespace Infrastructure.Data
         {
             return await _context.Products
             .Include(p =>p.ProductType)
-            .Include(p =>p.ProductBrand)
+             .Include(p =>p.ProductBrand)
                 .FirstOrDefaultAsync(p=> p.Id == id);
         }
 
@@ -33,8 +33,8 @@ namespace Infrastructure.Data
         {
             return await _context.Products
             .Include(p =>p.ProductType)
-            .Include(p =>p.ProductBrand)
-            .ToListAsync();;
+             .Include(p =>p.ProductBrand)
+              .ToListAsync();;
         }
 
         public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
