8ff29255b46bfd1a7c097d7b278f5e3c922ea3fe
Sifiso Tsukulu <sjtsukulu@shoprite.co.za>
2022-07-07 14:52:27 +0200

    End of section 6

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -27,7 +27,7 @@ namespace Infrastructure.Data
             return await _context.Set<T>().ToListAsync();
         }
 
-         public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
         {
             return await ApplySpecification(spec).FirstOrDefaultAsync();
         }
@@ -36,9 +36,15 @@ namespace Infrastructure.Data
         {
             return await ApplySpecification(spec).ToListAsync();
         }
-
-        private IQueryable<T> ApplySpecification(ISpecification<T> spec){
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
             return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
+
+
     }
 }
