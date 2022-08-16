fc688e6101f732a7b6b3dc42a876b0a928b7b079
Sifiso Tsukulu <sjtsukulu@shoprite.co.za>
2022-08-16 11:08:06 +0200

    End of section 14

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BasketService } from 'src/app/basket/basket.service';
+import { IBasket } from 'src/app/shared/models/basket';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent implements OnInit, AfterViewInit {
+  basket$ : Observable <IBasket>;
 
-  constructor() { }
+//  @ViewChild("cartCount") test: ElementRef;
+
+
+  constructor(private basketService: BasketService) { }
 
   ngOnInit(): void {
+    this.basket$ = this.basketService.basket$;
+  }
+
+  ngAfterViewInit(): void {
+  /*  console.log('this.cartCount after--->', this.test)
+    this.basket$.subscribe((basket: IBasket ) => { 
+      console.log('basket-->', basket)
+      this.test.nativeElement.setAttribute('data-count', basket?.items?.length || 0 )
+    })*/
+     //this.test.nativeElement.removeAttribute('data-counter');
   }
 
 }
