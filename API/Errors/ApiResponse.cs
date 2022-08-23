b97a1a513533ef2649f6a3cf7e518b011f696759
Sifiso Tsukulu <sjtsukulu@shoprite.co.za>
2022-08-23 14:38:40 +0200

    End of section 17

--- API/Errors/ApiResponse.cs
+++ API/Errors/ApiResponse.cs
@@ -19,7 +19,7 @@ namespace API.Errors
         {
             return statusCode switch{
                 400 => "A bad request you have made",
-                401 => "Authorizes, you are not",
+                401 => "Authorized, you are not",
                 404 => "Resource found, it was not",
                 500 => "Errors are the path to the dark side. Errors leads to anger. Anger leads to hate. Hate leads to career change",
                 _ => null
--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,32 +1,28 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AccountService } from 'src/app/account/account.service';
 import { BasketService } from 'src/app/basket/basket.service';
 import { IBasket } from 'src/app/shared/models/basket';
+import { IUser } from 'src/app/shared/models/user';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent implements OnInit, AfterViewInit {
+export class NavBarComponent implements OnInit {
   basket$ : Observable <IBasket>;
+  currentUser$: Observable<IUser>;
 
-//  @ViewChild("cartCount") test: ElementRef;
-
-
-  constructor(private basketService: BasketService) { }
+  constructor(private basketService: BasketService, private accountService: AccountService) { }
 
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
-  ngAfterViewInit(): void {
-  /*  console.log('this.cartCount after--->', this.test)
-    this.basket$.subscribe((basket: IBasket ) => { 
-      console.log('basket-->', basket)
-      this.test.nativeElement.setAttribute('data-count', basket?.items?.length || 0 )
-    })*/
-     //this.test.nativeElement.removeAttribute('data-counter');
-  }
+logout(){
+  this.accountService.logout();
+}
 
 }
